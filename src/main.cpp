fbdcb78e2766261fda20502389c7c5340c131170
MrFezaro <116104181+MrFezaro@users.noreply.github.com>
2025-10-27 15:57:14 +0100

    stable version

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,14 @@
   Complete project details at https://RandomNerdTutorials.com/getting-started-esp32-c3-super-mini/
 *********/
 
-// ESP32 C3 Super Mini on-board LED (works with inverted logic)
 const int ledPin = 8;
 
 void setup() {
+    Serial.begin(115200);
     pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-    Serial.begin(115200);
     digitalWrite(ledPin, HIGH);
     Serial.println("LED OFF");
     delay(1000);
