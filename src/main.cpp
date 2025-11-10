35fa11287e6c0aa114848ce4b818717ab48c87dc
MrFezaro <116104181+MrFezaro@users.noreply.github.com>
2025-11-10 16:16:16 +0100

    pwm and ir array works

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,39 @@
 #include <QTRSensors.h>
 #include <SparkFun_Qwiic_OTOS_Arduino_Library.h>
 #include <Wire.h>
+#include <Servo.h>
 
-int state = 0;
+Servo M1, M2;
+QTRSensors qtr;
 
-void setup() {
+constexpr uint8_t SensorCount = 2;
+uint16_t sensorValues[SensorCount];
+
+void setup()
+{
     Serial.begin(115200);
+    M1.attach(0);
+
+    qtr.setTypeRC();
+    qtr.setSensorPins((const uint8_t[]){1, 3}, SensorCount);
+
+    delay(500);
+    pinMode(8, OUTPUT);
+    digitalWrite(8, LOW);
+
+    for (uint16_t i = 0; i < 200; i++)
+    {
+        qtr.calibrate();
+    }
+    digitalWrite(8, HIGH);
 }
 
-void loop() {
+void loop(){
+    uint16_t position = qtr.readLineBlack(sensorValues);
 
+    Serial.println(position);
+
+    M1.writeMicroseconds(map(position, 0, 1000, 1000, 2000));
+
+    delay(10);
 }
