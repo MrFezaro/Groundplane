9ef591dc57bae6732c7f6a2e45b4cae560ec6d9f
MrFezaro <116104181+MrFezaro@users.noreply.github.com>
2025-11-17 16:27:59 +0100

    implemented semi working pid controller

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,14 @@
 #include <SparkFun_Qwiic_OTOS_Arduino_Library.h>
 #include <Wire.h>
 #include <Servo.h>
+#include "PIDino.hpp"
+
 
 Servo M1, M2;
 QTRSensors qtr;
+
+// Create a PIDino object with the following parameters:
+PIDino pid1(500, 0, 0, 10, 0.01, 0.01);
 
 constexpr uint8_t SensorCount = 2;
 uint16_t sensorValues[SensorCount];
@@ -15,7 +20,9 @@ uint16_t sensorValues[SensorCount];
 void setup()
 {
     Serial.begin(115200);
+
     M1.attach(0);
+    M2.attach(2);
 
     qtr.setTypeRC();
     qtr.setSensorPins((const uint8_t[]){1, 3}, SensorCount);
@@ -32,11 +39,15 @@ void setup()
 }
 
 void loop(){
-    uint16_t position = qtr.readLineBlack(sensorValues);
+    uint16_t input = qtr.readLineBlack(sensorValues);
 
-    Serial.println(position);
+    Serial.println(input);
 
-    M1.writeMicroseconds(map(position, 0, 1000, 1000, 2000));
+    const float output = pid1.compute(input); // Compute the PID output
+    Serial.println(output); // Print the output to the serial monitor
+
+    M1.writeMicroseconds(output);
+    M2.writeMicroseconds(-output);
 
     delay(10);
 }
